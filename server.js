fde675502f51a1d94fed2ff537b3741800a0beb7
Sarah Dowd <sarahlynnedowd@gmail.com>
2021-01-15 20:22:16 -0500

    required path

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 const { animals } = require('./data/animals');
+const path = require('path');
 
 function filterByQuery(query, animalsArray) {
     let filteredResults = animalsArray;
